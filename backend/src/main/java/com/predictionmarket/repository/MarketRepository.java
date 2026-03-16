5ad7cfefeacd0f4adeeac313f6be46e03df36908
Morollite <morollite@gmail.com>
2026-03-16 15:29:19 -0500

    added Bet

--- backend/src/main/java/com/predictionmarket/repository/MarketRepository.java
+++ backend/src/main/java/com/predictionmarket/repository/MarketRepository.java
@@ -3,6 +3,6 @@ package com.predictionmarket.repository;
 import com.predictionmarket.model.Market;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
 public interface MarketRepository extends JpaRepository<Market, Long> {
+    //
 }
